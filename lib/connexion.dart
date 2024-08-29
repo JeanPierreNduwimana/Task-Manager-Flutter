279e02c2029d12cc1546e2bb4b6ba2cf837cdf4e
Jean Pierre Nduwimana <1783527@cegepmontpetit.ca>
2024-08-29 13:27:18 -0400

    Mise en place de l'interface de la page inscription et connexion.
    La page connexion et inscription contiennent leur champs et les boutons respectives.

--- lib/connexion.dart
+++ lib/connexion.dart
@@ -7,7 +7,8 @@ class Connexion extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
+    final TextEditingController username_controller = TextEditingController();
+    final TextEditingController password_controller = TextEditingController();
     return Scaffold(
 
       appBar: AppBar(
@@ -16,14 +17,35 @@ class Connexion extends StatelessWidget {
       ),
 
       body: Center(
-          child: Column(
-            children: [
-              const Text('Ceci est la page de Connexion'),
-              ElevatedButton(
-                child: const Text('Acceuil'),
-                onPressed: () {Navigator.pushNamed(context, '/accueil');},
-              )
-            ],
+          child: Padding(
+            padding: const EdgeInsets.all(48.0),
+            child: Column(
+              children: [
+                const Text('Connexion'),
+                TextField(
+                  controller: username_controller,
+                  keyboardType: TextInputType.name,
+                  maxLength: 16,
+                  decoration: const InputDecoration(
+                      hintText: 'Username',
+                      hintStyle: TextStyle(color: Colors.black38)
+                  ),
+                ),
+                TextField(
+                  controller: password_controller,
+                  obscureText: true,
+                  keyboardType: TextInputType.visiblePassword,
+                  decoration: const InputDecoration(
+                      hintText: 'Password',
+                      hintStyle: TextStyle(color: Colors.black38)
+                  ),
+                ),
+                ElevatedButton(
+                  child: const Text('Accueil'),
+                  onPressed: () {Navigator.pushNamed(context, '/accueil');},
+                )
+              ],
+            ),
           )
       ),
     );
--- lib/inscription.dart
+++ lib/inscription.dart
@@ -8,6 +8,11 @@ class Inscription extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+    final TextEditingController username_controller = TextEditingController();
+    final TextEditingController password_controller = TextEditingController();
+    final TextEditingController confirm_password_controller = TextEditingController();
+
+
     return Scaffold(
 
       appBar: AppBar(
@@ -17,14 +22,46 @@ class Inscription extends StatelessWidget {
 
       body: Center(
 
-        child: Column(
-          children: [
-            const Text('Ceci est la page d\'inscription'),
-            ElevatedButton(
-              child: const Text('Connexion'),
-              onPressed: () {Navigator.pushNamed(context, '/connexion');},
-            )
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(48.0),
+          child: Column(
+            children: [
+              const Text('inscription'),
+              TextField(
+                controller: username_controller,
+                keyboardType: TextInputType.name,
+                maxLength: 16,
+                decoration: const InputDecoration(
+                  hintText: 'username',
+                  hintStyle: TextStyle(color: Colors.black38)
+                ),
+              ),
+              TextField(
+                controller: password_controller,
+                obscureText: true,
+                keyboardType: TextInputType.visiblePassword,
+                decoration: const InputDecoration(
+                    hintText: 'password',
+                    hintStyle: TextStyle(color: Colors.black38)
+                ),
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                controller: confirm_password_controller,
+                obscureText: true,
+                keyboardType: TextInputType.visiblePassword,
+                decoration: const InputDecoration(
+                    hintText: 'confirm your password',
+                    hintStyle: TextStyle(color: Colors.black38)
+                ),
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                child: const Text('Connexion'),
+                onPressed: () {Navigator.pushNamed(context, '/connexion');},
+              )
+            ],
+          ),
         )
       ),
       );
